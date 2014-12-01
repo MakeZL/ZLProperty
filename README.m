12967f2054bd1e171d62ff3fd5107c41adaba4fd
zhangleo <zhangleowork@163.com>
2014-12-01 19:37:15 +0800

    add README. Fix some a little code

--- README.m
+++ README.m
@@ -1,30 +1,4 @@
-//
-//  ViewController.m
-//  ZLProperty
-//
-//  Created by 张磊 on 14-12-1.
-//  Copyright (c) 2014年 com.zixue101.www. All rights reserved.
-//
-
-#import "ViewController.h"
-#import "Person.h"
-#import "Clother.h"
-#import "Animal.h"
-#import "NSObject+ZLProperty.h"
-
-@interface ViewController ()
-
-@property (nonatomic , strong) NSMutableDictionary *dictM;
-
-@end
-
-@implementation ViewController
-
-- (void)viewDidLoad {
-    [super viewDidLoad];
-     
-    // 衣服模型，人可以拥有多件衣服，狗也能拥有多件衣服
-    NSArray *clothes = @[
+Array *clothes = @[
                          @{@"name":@"Meters/Bonwe",@"price":@(125)},
                          @{@"name":@"nike",@"price":@(255)},
                          ];
@@ -51,6 +25,3 @@
         NSLog(@"%@ -- %ld",animal.name , animal.age);
     }
 
-}
-
-@end
